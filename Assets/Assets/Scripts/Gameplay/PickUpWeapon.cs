cb8092b36d50b108084d26a40ec45a58f5838463
CamatoDev <mrctomba@gmail.com>
2024-07-19 18:39:56 +0100

    Update PickUpWeapon
    Le script est maintenant sur la main armée du personnage pour facilité la gestion et le remplacement des armes.

--- Assets/Assets/Scripts/Gameplay/PickUpWeapon.cs
+++ Assets/Assets/Scripts/Gameplay/PickUpWeapon.cs
@@ -6,6 +6,7 @@ public class PickUpWeapon : MonoBehaviour
 {
     //Reférence au joueur 
     public Transform player;
+    public Transform player_hand;
 
     //Numéro d'orde des armes 
     int mainWeapon = 0;
@@ -20,22 +21,27 @@ public class PickUpWeapon : MonoBehaviour
     {
         //On passe le joueur dans la variable 
         player = GameObject.FindGameObjectWithTag("Player").transform;
-
-        //L'arme secondaire est définit comme celle qu'on veut ramasser 
-        Player_inventory.weapons[secondaryWeapon] = gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Distance entre l'arme et le joueur 
-        float Distance = Vector3.Distance(transform.position, player.position);
+        //Tableau des armes présent au terrain 
+        GameObject[] allWeapon = GameObject.FindGameObjectsWithTag("weapon");
 
-        //Ramasse de l'arme si le joueur est à la bonne distance 
-        if(Distance <= pickUpRange)
+        foreach (var weapon in allWeapon)
         {
+            //Distance entre l'arme et le joueur 
+            float Distance = Vector3.Distance(weapon.transform.position, player.position);
 
-            MainWeaponChange();
+            //Ramasse de l'arme si le joueur est à la bonne distance 
+            if (Distance <= pickUpRange)
+            {
+                //L'arme secondaire est définit comme celle qu'on veut ramasser 
+                Player_inventory.weapons[secondaryWeapon] = weapon;
+
+                MainWeaponChange();
+            }
         }
     }
 
@@ -43,21 +49,21 @@ public class PickUpWeapon : MonoBehaviour
     void MainWeaponChange()
     {
         //L'arme deviens enfant de la main 
-        transform.SetParent(Player_inventory.weapons[mainWeapon].transform);
+        Player_inventory.weapons[secondaryWeapon].transform.SetParent(transform);
         //Changement de position et de rotation
-        transform.position = Player_inventory.weapons[mainWeapon].transform.position;
-        transform.rotation = Player_inventory.weapons[mainWeapon].transform.rotation;
+        Player_inventory.weapons[secondaryWeapon].transform.position = Player_inventory.weapons[mainWeapon].transform.position;
+        Player_inventory.weapons[secondaryWeapon].transform.rotation = Player_inventory.weapons[mainWeapon].transform.rotation;
 
         //Changement d'ordre 
         Player_inventory.weapons[temp] = Player_inventory.weapons[mainWeapon];
         Player_inventory.weapons[mainWeapon] = Player_inventory.weapons[secondaryWeapon];
-        Player_inventory.weapons[secondaryWeapon] = Player_inventory.weapons[mainWeapon];
+        Player_inventory.weapons[secondaryWeapon] = Player_inventory.weapons[temp];
 
         //Suppression de l'ancienne arme 
         Destroy(Player_inventory.weapons[secondaryWeapon]);
     }
 
-    private void OnDrawGizmos()
+    private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.green;
         Gizmos.DrawSphere(transform.position, pickUpRange);
